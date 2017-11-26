5884dad5d0f977b6f7cfe1346cae1b85332bf517
Naoya Sawada <naoya@tuntunkun.com>
2017-11-26 17:00:01 +0900

    add httpd

---
+++ httpd.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pyramid.config import Configurator
+from waitress import serve
+
+#
+# Main
+#
+if __name__ == '__main__':
+	#
+	# WEB アプリケーションの設定
+	#
+	config = Configurator()
+
+	from driver.bacnet import BACnetd
+	config.registry.bacnetd = BACnetd('10.2.10.17/24')
+	config.registry.bacnetd.start()
+
+	#
+	# API
+	#
+	import api.bacnet
+	config.include(api.bacnet.bootstrap, route_prefix='api/bacnet/')
+	import api.icmp
+	config.include(api.icmp.bootstrap, route_prefix='api/icmp/')
+
+	#
+	# HTTPDサーバの設定
+	#
+	app = config.make_wsgi_app()
+	serve(app, host='0.0.0.0', port=8080)
+
