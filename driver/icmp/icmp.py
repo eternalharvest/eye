4f46bd2c8e78a9aac338ab3676feebba2d4c9197
Naoya Sawada <naoya@tuntunkun.com>
2017-11-26 20:06:22 +0900

    modify icmp result format

--- driver/icmp/icmp.py
+++ driver/icmp/icmp.py
@@ -21,15 +21,18 @@ class ICMP:
 		if r.ret_code == 0:
 			return {
 				'result'	: True,			# 結果
-				'destination'	: r.destination,	# 送り先
-				'destination_ip': r.destination_ip,	# 送り先IP
-				'min_rtt'	: r.min_rtt,		# 最小往復秒
-				'max_rtt'	: r.max_rtt,		# 最大往復秒
-				'avg_rtt'	: r.avg_rtt,		# 平均往復秒
+				'values'	: [
+					'destination'	: r.destination,	# 送り先
+					'destination_ip': r.destination_ip,	# 送り先IP
+					'min_rtt'	: r.min_rtt,		# 最小往復秒
+					'max_rtt'	: r.max_rtt,		# 最大往復秒
+					'avg_rtt'	: r.avg_rtt,		# 平均往復秒
+				]
 			}
 		else:
 			return {
 				'result'	: False,		# 結果
+				'values'	: []
 			}
 
 if __name__ == '__main__':
