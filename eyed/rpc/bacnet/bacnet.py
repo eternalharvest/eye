1bb4315174cd6bac3ae323be8745a508c06dd213
Naoya Sawada <naoya@tuntunkun.com>
2018-01-28 15:33:38 +0900

    bacnet リクエスト用のクラス追加

---
+++ eyed/rpc/bacnet/bacnet.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# BACnet Driver
+#
+from driver.bacnet import BACnetSimpleClient
+from driver.bacnet import definition
+
+#
+# BACnet Daemon Instance
+#
+from eyed.rpc.bacnetd import SingleBACnetd
+
+#
+# BACnetService
+#
+class BACnetService(object):
+	#
+	# WhoIsRequest の 実行
+	#
+	def exposed_doWhoIsRequest(self, timeout = 10):
+		#
+		# BACnet コマンド操作用インスタンス取得
+		#
+		single = SingleBACnetd.getInstance()
+		app = single.bacnetd.application
+		bacnet = BACnetSimpleClient(app)
+
+		#
+		# WhoIsRequest の 送信
+		#
+		bacnet.WhoIsRequest()
+
+		#
+		# WhoIsRequest を 投げてから最初の IAmRequestを受け取るまで待つ
+		#
+		try:
+			device_id = app.responseQueue.get(timeout = timeout)
+			return { 'device_id' : device_id }
+		except Exception:
+			#
+			# タイムアウトを通知
+			#
+			return None
+
+	#
+	# デバイスマップの取得
+	#
+	def exposed_getDevices(self):
+		#
+		# BACnet コマンド操作用インスタンス取得
+		#
+		single = SingleBACnetd.getInstance()
+		app = single.bacnetd.application
+		bacnet = BACnetSimpleClient(app)
+
+		#
+		# デバイスリストの作成
+		#
+		devices = []
+		for key, value in app.device_map.items():
+			devices.append({ 'device_id' : key, 'ip' : str(value) })
+
+		#
+		# デバイスリストを返却
+		#
+		return devices
+
+	#
+	# ReadPropertyRequest の 実行
+	#
+	def exposed_doReadPropertyRequest(self, device_id, object_id, instance_id, property_id):
+		#
+		# BACnet コマンド操作用インスタンス取得
+		#
+		single = SingleBACnetd.getInstance()
+		app = single.bacnetd.application
+		bacnet = BACnetSimpleClient(app)
+
+		#
+		# リクエストの実行
+		#
+		value = bacnet.ReadPropertyRequest(device_id, object_id, instance_id, property_id)
+
+		#
+		# リクエスト結果をJSONで返す
+		#
+		return { 'value' : value }
+
