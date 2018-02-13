d807f1dc0a5ea8c98287bd1c6ecc5790fd5d09c6
Naoya Sawada <naoya@tuntunkun.com>
2018-02-14 00:20:49 +0900

    AnalogInput

--- eyed/driver/bacnet/proxy/__init__.py
+++ eyed/driver/bacnet/proxy/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from object import ProxyAnalogValueObject
+from object import ProxyAnalogInputObject
 
-__all__ = [ProxyAnalogValueObject]
+__all__ = [
+	ProxyAnalogValueObject,
+	ProxyAnalogInputObject
+]
 
--- eyed/driver/bacnet/proxy/object.py
+++ eyed/driver/bacnet/proxy/object.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from bacpypes.object import AnalogValueObject
+from bacpypes.object import AnalogValueObject, AnalogInputObject
 from bacpypes.object import register_object_type
 from property import ProxyValueProperty
 
 #
-# Proxy Value Object Type
+# Proxy Analog Value Object Type
 #
 class ProxyAnalogValueObject(AnalogValueObject):
 	properties = [
@@ -16,7 +16,19 @@ class ProxyAnalogValueObject(AnalogValueObject):
 		AnalogValueObject.__init__(self, **kwargs)
 
 #
+# Proxy Analog Input Object Type
+#
+class ProxyAnalogInputObject(AnalogInputObject):
+	properties = [
+		ProxyValueProperty('presentValue')
+	]
+
+	def __init__(self, **kwargs):
+		AnalogInputObject.__init__(self, **kwargs)
+
+#
 # オブジェクトを登録
 #
 register_object_type(ProxyAnalogValueObject)
+register_object_type(ProxyAnalogInputObject)
 
