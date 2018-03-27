838ff370ffba26e807d3c7be69565cdcd435cf42
Naoya Sawada <naoya@tuntunkun.com>
2018-03-27 12:17:41 +0900

    ドキュメントの追加

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: 'v0.4.0',
+    LANGUAGE: 'jp',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
